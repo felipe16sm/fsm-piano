6b2805413bbe60683842138c6a71899f171ee8cd
Felipe Sobrinho Molina <felipe16sm@gmail.com>
2026-04-19 16:01:20 -0300

    feat: implementação de rate limiter

--- client/src/hooks/useMusic/index.ts
+++ client/src/hooks/useMusic/index.ts
@@ -20,6 +20,7 @@ const useMusic = ({ id }: { id?: string }) => {
         const { data } = await api.get("/api/musics");
         return data;
       },
+      retry: false,
     }),
     getOneMusic: useQuery({
       queryKey: ["music", id],
@@ -27,24 +28,28 @@ const useMusic = ({ id }: { id?: string }) => {
         const { data } = await api.get(`/api/musics/${id}`);
         return data;
       },
+      retry: false,
     }),
     createMusic: useMutation({
       mutationFn: async (data: MusicPayload) => {
         const response = await api.post("/api/musics", data);
         return response.data;
       },
+      retry: false,
     }),
     updateMusic: useMutation({
       mutationFn: async (data: MusicPayload) => {
         const response = await api.put(`/api/musics/${id}`, data);
         return response.data;
       },
+      retry: false,
     }),
     deleteMusic: useMutation({
       mutationFn: async (data: { id: string }) => {
         const response = await api.delete(`/api/musics/${data.id}`);
         return response.data;
       },
+      retry: false,
     }),
   };
 };
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,16 +2,33 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import rateLimit from "express-rate-limit";
 
 import { connectDB } from "./db";
 import musicRoutes from "./routes";
+
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 15 min
+  max: 30,
+
+  handler: (req: any, res: any) => {
+    const retryAfter = Math.ceil(
+      (req.rateLimit.resetTime.getTime() - Date.now()) / 1000,
+    );
+
+    res.status(429).json({
+      error: "Muitas requisições",
+      retryAfter,
+    });
+  },
+});
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/musics", musicRoutes);
+app.use("/api/musics", limiter, musicRoutes);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
